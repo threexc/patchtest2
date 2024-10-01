48ff2254604063580dd0ec9b8c9fdae9bc4e1b7a
Trevor Gamblin <tgamblin@baylibre.com>
2024-09-30 20:52:44 -0400

    patchtest2: build out basic testing logic
    Add:
    - test_for_pattern()
    - PatchtestResult class, including __str__() method
    - PatchtestFail class, including __str__() method
    - PatchtestResults class
    - test_mbox_signed_off_by_presence() demo test
    - patchtest.py call to loop over mbox and get
      test_mbox_signed_off_by_presence() results
    
    Signed-off-by: Trevor Gamblin <tgamblin@baylibre.com>

--- src/patchtest2/tests/core.py
+++ src/patchtest2/tests/core.py
@@ -1,12 +1,42 @@
-from dataclasses import dataclass
+import patchtest2.patterns as patterns
 
-@dataclass
 class PatchtestResult:
-    patch: str
-    testname: str
-    result: str
-    reason: str
+    def __init__(self, patch, testname, result):
+        self.patch = patch
+        self.testname = testname
+        self.result = result
 
-def test_for_pattern(pattern, target):
-    pass
+    def __str__(self):
+        return f"{self.result}: {self.testname} on {self.patch}"
 
+class PatchtestFail(PatchtestResult):
+    def __init__(self, patch, testname, result, reason):
+        super().__init__(patch, testname, result)
+        self.reason = reason
+
+    def __str__(self):
+        return f"{self.result}: {self.testname} on {self.patch} ({self.reason})"
+
+class PatchtestResults:
+    def __init__(self, target_repo, series):
+        self.target_repo = target_repo
+        self.series = series
+        self.mbox_signed_off_by_results = [test_mbox_signed_off_by_presence(patch) for patch in self.series.patchdata]
+
+# test_for_pattern()
+# @pattern: a pyparsing regex object
+# @string: a string (patch subject, commit message, author, etc. to
+# search for
+def test_for_pattern(pattern, string):
+    if pattern.search_string(string):
+        return "PASS"
+    else:
+        return "FAIL"
+
+def test_mbox_signed_off_by_presence(target):
+    result = test_for_pattern(patterns.signed_off_by,
+                              target.commit_message)
+    if result == "PASS":
+        return PatchtestResult(target.subject, "mbox_signed_off_by_presence", result)
+    elif result == "FAIL":
+        return PatchtestFail(target.subject, "mbox_signed_off_by_presence", result, "mbox was missing a signed-off-by tag")
--- src/patchtest2/tests/patchtest.py
+++ src/patchtest2/tests/patchtest.py
@@ -1,7 +1,12 @@
 from patchtest2.parser import PatchtestParser
+from patchtest2.mbox import PatchSeries, TargetRepo
+from patchtest2.tests.core import PatchtestResults
 
 def run():
     parser = PatchtestParser.get_parser()
     args = parser.parse_args()
-
-    print(args)
+    target_repo = TargetRepo(args.repodir)
+    series = PatchSeries(args.patch_path)
+    results = PatchtestResults(target_repo, series)
+    for testresult in results.mbox_signed_off_by_results:
+        print(testresult)
